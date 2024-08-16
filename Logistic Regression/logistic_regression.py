81ec4a338dcfe079de32833c175f6e66d2064bc6
abhisekg9516@gmail.com <abhisekg9516@gmail.com>
2024-08-16 12:04:40 +0530

    adding logistic regression

--- Logistic Regression/logistic_regression.py
+++ Logistic Regression/logistic_regression.py
@@ -4,36 +4,28 @@ from enum import Enum
 from numba import jit
 
 
-class SolverMethod(Enum):
-    """Enumeration for solver methods in linear regression."""
-    NORMAL_EQUATION = 0  # Method using normal equations
-    SGD = 1  # Method using stochastic gradient descent
+class LogisticRegression:
+    """Logistic Regression model class implementing both normal equation and SGD methods."""
 
-
-class LinearRegression:
-    """Linear Regression model class implementing both normal equation and SGD methods."""
-
-    def __init__(self, method=SolverMethod.NORMAL_EQUATION,
+    def __init__(self,
                  epochs: int = 100,
                  learning_rate: float = 0.01,
                  batch_size: int = 32) -> None:
         """
-        Initialize the Linear Regression model.
+        Initialize the Logistic Regression model.
 
         Parameters:
-        method (SolverMethod): Method to use for fitting the model.
         epochs (int): Number of epochs for SGD.
         learning_rate (float): Learning rate for SGD.
         batch_size (int): Batch size for SGD.
         """
-        self.method = method
         self.epochs = epochs
         self.learning_rate = learning_rate
         self.batch_size = batch_size
 
     def fit(self, X: np.array, y: np.array):
         """
-        Fit the Linear Regression model to the training data.
+        Fit the Logistic Regression model to the training data.
 
         Parameters:
         X (np.array): Feature matrix.
@@ -42,12 +34,10 @@ class LinearRegression:
         self.num_rows, self.num_cols = X.shape
         # Add a bias term (column of ones) to the feature matrix
         self.X_b = np.concatenate([np.ones((self.num_rows, 1)), X], axis=1)
-        if self.method == SolverMethod.NORMAL_EQUATION:
-            self.intercept, self.coefs = self._fit_normal_equation(self.X_b, y)
-        elif self.method == SolverMethod.SGD:
-            param_arr = np.random.randn(self.num_cols + 1, 1)
-            self.intercept, self.coefs = fit_sgd(
-                self.X_b, y, param_arr, self.learning_rate, self.epochs, self.batch_size)
+
+        param_arr = np.random.randn(self.num_cols + 1, 1)
+        self.intercept, self.coefs = fit_sgd(
+            self.X_b, y, param_arr, self.learning_rate, self.epochs, self.batch_size)
 
     def predict(self, X):
         """
@@ -62,24 +52,17 @@ class LinearRegression:
         # Compute the predicted values using the learned coefficients
         # Add a bias term to the feature matrix
         X_b = np.concatenate([np.ones((X.shape[0], 1)), X], axis=1)
-        return X_b @ np.concatenate([[self.intercept], self.coefs],
-                                    axis=0)
+        return sigmoid(X_b @ np.concatenate([[self.intercept], self.coefs],
+                                             axis=0))
 
-    def _fit_normal_equation(self, X, y):
-        """
-        Fit the model using the normal equation method.
 
-        Parameters:
-        X (np.array): Feature matrix with bias term.
-        y (np.array): Target vector.
-
-        Returns:
-        float, np.array: Intercept and coefficients of the model.
-        """
-        # Compute the optimal coefficients using the normal equation
-        param_arr = np.linalg.inv(X.T @ X) @ X.T @ y
-        intercept, coefs = param_arr[0], param_arr[1:]
-        return intercept, coefs
+@jit(nopython=True, fastmath=True)
+def sigmoid(arr: np.array) -> np.array:
+    return np.where(
+        arr >= 0,  # condition
+        1 / (1 + np.exp(-arr)),  # For positive values
+        np.exp(arr) / (1 + np.exp(arr))  # For negative values
+    )
 
 
 @jit(nopython=True, fastmath=True)
@@ -132,7 +115,7 @@ def calculate_batch_gradients(X_batch: np.array, y_batch: np.array, param_arr: n
     #  2*(y_hat - y)@X/sample_size = 2*(X_T@param_arr - y)@X/sample_size = 2*error@X/sample_size
 
     # (bs x num_cols) x (num_cols x 1 ) = (bs x 1)
-    prediction = X_batch @ param_arr
+    prediction = sigmoid(X_batch @ param_arr)
 
     error = prediction - y_batch
     # (bs x num_cols).T x (bs x 1)  = (num_cols x 1)
--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Implementing machine learning algorithms from scratch involves coding the core l
 
 Algorithms Implemented:
 
-- [K-Nearest Neighbors (KNN)](./KNN%20Regressor/knn_regressor.py)
 - [Linear Regression](./Linear%20Regression/linear_regression.py)
+- [Logistic Regression](./Logistic%20Regression/logistic_regression.py)
+- [K-Nearest Neighbors (KNN)](./KNN%20Regressor/knn_regressor.py)
 - [Decision Trees](./DecisionTree/decision_tree_regressor.py)
 
 
